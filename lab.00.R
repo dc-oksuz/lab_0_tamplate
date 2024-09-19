b3250348f46e9cb228cb9b260453c161941dc627
dc-oksuz <doksuz@ed.ac.uk>
2024-09-19 23:38:41 +0100

    commit

---
+++ lab.00.R
@@ -0,0 +1,33 @@
+library(tidyverse) 
+
+team_data <- tibble(
+  name = c("Peter Rabbit", "Bugs Bunny", "White Rabbit", 
+           "Judy Hopps", "Thumper"),
+  github_username = c("mcgregors-garden", "whats-up-doc", "Im-late", 
+                      "have-to-try", "say-nothing"), 
+  colour = c("powderblue", "darkorange", "white", 
+             "seagreen1", "plum"),
+  hobby = c("Hopping", "Comedy", "Tailoring", 
+            "Driving", "Drumming"),
+  favourite_number = c(2, 8, 5, 7, 10)
+)
+
+team_data
+
+p1 <- ggplot(data = team_data, 
+             mapping = aes(x = github_username, 
+                           y = favourite_number,
+                           fill = colour), 
+) +
+  geom_col() +
+  scale_fill_identity()
+p1
+
+p1 + labs(x = "GitHub Username", 
+          y = "Favourite Number",
+          title = "Data collected from team Rabbit")
+
+p2 <- ggplot(team_data, aes(x = hobby)) +
+  geom_bar()
+p2
+
