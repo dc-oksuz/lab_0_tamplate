c692034b774931f5359aad30d3988ba31f744b8e
dc-oksuz <doksuz@ed.ac.uk>
2024-09-19 23:35:07 +0100

    commit more

--- lab_0.R
+++ lab_0.R
@@ -12,3 +12,18 @@ team_data <- tibble(
   favourite_number = c(2, 8, 5, 7, 10)
 )
 team_data
+
+p1 <- ggplot(data = team_data, 
+             mapping = aes(x = github_username, 
+                           y = favourite_number,
+                           fill = colour), 
+) +
+  geom_col() +
+  scale_fill_identity()
+p1
+
+p1 + labs(x = "GitHub Username", 
+          y = "Favourite Number",
+          title = "Data collected from team Rabbit")
+
+
